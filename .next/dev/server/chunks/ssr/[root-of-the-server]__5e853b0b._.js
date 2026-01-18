404a20a717a36fe89a92769028e43e498ed9fb7e
dhanukaprr <37775879+dhanukaprr@users.noreply.github.com>
2026-01-18 22:12:11 +0530

    Database Fix

--- .next/dev/server/chunks/ssr/[root-of-the-server]__5e853b0b._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__5e853b0b._.js
@@ -730,8 +730,8 @@ const metadata = {
     description: "Codexea is your partner for digital growth. We specialize in custom Web Development, SEO, Social Media Strategy, and Creative Branding.",
     keywords: "Web Development, Digital Marketing, SEO, Social Media Marketing, Codexea, Sri Lanka, UI/UX Design, Branding"
 };
-function Home() {
-    const projects = (0, __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$data$2f$projects$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["getProjects"])();
+async function Home() {
+    const projects = await (0, __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$data$2f$projects$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["getProjects"])();
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Fragment"], {
         children: [
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$components$2f$EasterEgg$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
